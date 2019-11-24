8a449bed46130b9e21c2972c2c806111bafd3cf3
Mauro Merconchini <mauro_merconchini@yahoo.com>
2019-11-23 23:43:16 -0500

    Text draws, uses custom font, adapts to window size

--- QuakeMappingCompanion.pde
+++ QuakeMappingCompanion.pde
@@ -1,6 +1,7 @@
 
-//Initialize an array of strings that will hold the .map file line by line
+//Initialize an array of strings that will hold the map file line by line
 String mapFileLines[];
+PFont alata;
 
 void setup()
 {
@@ -10,16 +11,28 @@ void setup()
     //Initialize a 3D window for text and graphics to be displayed
     size(800, 600, P3D);
 
-    mapFileLines = loadStrings("endofsolace.map");
+    //Populate the array of strings with the lines of the map file
+    mapFileLines = loadStrings("data/endofsolace.map");
 }
 
 void draw()
 {
     cls();
+    fontSetup();
+    
+    textAlign(CENTER);
+    text("Hello World!", width/2, height/2);
 }
 
 //A simple method to clear the screen on each frame, avoid ghosting
 void cls()
 {
     background(0);
+}
+
+void fontSetup()
+{
+    //Load the font used for writing text
+    alata = createFont("Alata-Regular.ttf", width/10);
+    textFont(alata);
 }
--- out/source/QuakeMappingCompanion.java
+++ out/source/QuakeMappingCompanion.java
@@ -15,8 +15,9 @@ import java.io.IOException;
 public class QuakeMappingCompanion extends PApplet {
 
 
-//Initialize an array of strings that will hold the .map file line by line
+//Initialize an array of strings that will hold the map file line by line
 String mapFileLines[];
+PFont alata;
 
 public void setup()
 {
@@ -26,20 +27,30 @@ public void setup()
     //Initialize a 3D window for text and graphics to be displayed
     
 
-    mapFileLines = loadStrings("endofsolace.map");
+    //Populate the array of strings with the lines of the map file
+    mapFileLines = loadStrings("data/endofsolace.map");
 }
 
 public void draw()
 {
     cls();
-
-    text("Hello World!", 100, 100);
+    fontSetup();
+    
+    textAlign(CENTER);
+    text("Hello World!", width/2, height/2);
 }
 
 //A simple method to clear the screen on each frame, avoid ghosting
 public void cls()
 {
     background(0);
+}
+
+public void fontSetup()
+{
+    //Load the font used for writing text
+    alata = createFont("Alata-Regular.ttf", width/10);
+    textFont(alata);
 }
   public void settings() {  size(800, 600, P3D); }
   static public void main(String[] passedArgs) {
