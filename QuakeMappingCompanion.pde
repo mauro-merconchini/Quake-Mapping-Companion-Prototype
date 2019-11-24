282ac532a607063b83c5b9105c6227f54322594f
Mauro Merconchini <mauro_merconchini@yahoo.com>
2019-11-23 22:45:54 -0500

    Added gitignore file

--- QuakeMappingCompanion.pde
+++ QuakeMappingCompanion.pde
@@ -1,7 +1,7 @@
 void setup()
 {
     //This makes sure that the window can be resized, both in 2D and 3D
-    surface.setResizeable(True);
+    surface.setResizable(true);
 
     //Initialize a 3D window for text and graphics to be displayed
     size(800, 600, P3D);
--- out/source/QuakeMappingCompanion.java
+++ out/source/QuakeMappingCompanion.java
@@ -14,43 +14,20 @@ import java.io.IOException;
 
 public class QuakeMappingCompanion extends PApplet {
 
-PShape myShape;
-
-float rotation = 0;
-
 public void setup()
 {
-    
-    myShape = loadShape("teapot.obj");
-
+    //This makes sure that the window can be resized, both in 2D and 3D
     surface.setResizable(true);
-    background(75);
 
-    stroke(255);
+    //Initialize a 3D window for text and graphics to be displayed
+    
 }
 
 public void draw()
 {
-    ambientLight(102, 102, 102);
-    background(75);
-    // translate(width/2, height/2);
-    // shape(myShape, 0, 0);
-    // box(75);
 
-    float orbitRadius= mouseX/2+50;
-    float ypos= mouseY/3;
-    float xpos= cos(radians(rotation))*orbitRadius;
-    float zpos= sin(radians(rotation))*orbitRadius;
-    
-    camera(xpos, ypos, zpos, 0, 0, 0, 0, -1, 0);
-
-    shape(myShape, 0, 0);
-
-    rotation+=0.5f;
-
-    
 }
-  public void settings() {  size(1280, 720, P3D); }
+  public void settings() {  size(800, 600, P3D); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "QuakeMappingCompanion" };
     if (passedArgs != null) {
